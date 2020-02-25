abb4310590f9494a41ad06db50b1b24bcdc91bf7
Jorge Eguiguren <jorgeeguiguren@Jorges-MacBook-Air.local>
2020-02-24 23:12:05 -0500

    yet another tweak for gh pages

---
+++ docs/precache-manifest.21eae8b9c26aeb5403c7982ec2ad56ba.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65db493ba1617c5d4d788bf3f1448bb5",
+    "url": "/gbholders/index.html"
+  },
+  {
+    "revision": "88615317c6f822231d9f",
+    "url": "/gbholders/static/css/main.4298a2c3.chunk.css"
+  },
+  {
+    "revision": "4ed53ad033e921754807",
+    "url": "/gbholders/static/js/2.786c1ed1.chunk.js"
+  },
+  {
+    "revision": "88615317c6f822231d9f",
+    "url": "/gbholders/static/js/main.499be2ea.chunk.js"
+  },
+  {
+    "revision": "3cd4d99bfcf2bc0eaec3",
+    "url": "/gbholders/static/js/runtime~main.4860e6ce.js"
+  },
+  {
+    "revision": "c0df655e289ea8ee4790b334b761fa7d",
+    "url": "/gbholders/static/media/CompanyLogo.c0df655e.png"
+  },
+  {
+    "revision": "5199462c37e44a2aaa0e288d081c73ab",
+    "url": "/gbholders/static/media/Condor.5199462c.png"
+  },
+  {
+    "revision": "cdc8975acb05a44d33cad30c931118a9",
+    "url": "/gbholders/static/media/DefaultNews.cdc8975a.jpg"
+  },
+  {
+    "revision": "cf7664eb1387af46cf1a639087690aa9",
+    "url": "/gbholders/static/media/Partnership.cf7664eb.png"
+  },
+  {
+    "revision": "2653d2bd4fa384b4012dcbec39a9c272",
+    "url": "/gbholders/static/media/Pronaca.2653d2bd.png"
+  },
+  {
+    "revision": "1b5e394e82ed445a5e8d104eb60638bd",
+    "url": "/gbholders/static/media/Unifer.1b5e394e.png"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/gbholders/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/gbholders/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/gbholders/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/gbholders/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "88be4d378a639ed51c8900beab042bde",
+    "url": "/gbholders/static/media/eduardo.88be4d37.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/gbholders/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/gbholders/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/gbholders/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/gbholders/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/gbholders/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
