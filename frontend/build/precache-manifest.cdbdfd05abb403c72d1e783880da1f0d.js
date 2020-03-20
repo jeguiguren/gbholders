8a21d2b1be17220ccb0e6667eb7a156cda7801fb
Jorge Eguiguren <jorgeeguiguren@Jorges-MacBook-Air.local>
2020-03-20 13:38:18 -0400

    renamed

---
+++ frontend/build/precache-manifest.cdbdfd05abb403c72d1e783880da1f0d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9517fcc9476b2f755983b6e7999db147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77384cef104f5f6d75a",
+    "url": "/static/css/main.e802564c.chunk.css"
+  },
+  {
+    "revision": "e9b4fd68b7391456a792",
+    "url": "/static/js/2.15fb3a54.chunk.js"
+  },
+  {
+    "revision": "f77384cef104f5f6d75a",
+    "url": "/static/js/main.e874b4bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0df655e289ea8ee4790b334b761fa7d",
+    "url": "/static/media/CompanyLogo.c0df655e.png"
+  },
+  {
+    "revision": "cdc8975acb05a44d33cad30c931118a9",
+    "url": "/static/media/DefaultNews.cdc8975a.jpg"
+  },
+  {
+    "revision": "cf7664eb1387af46cf1a639087690aa9",
+    "url": "/static/media/Partnership.cf7664eb.png"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "88be4d378a639ed51c8900beab042bde",
+    "url": "/static/media/eduardo.88be4d37.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
---
+++ frontend/src/index.js
@@ -0,0 +1,53 @@
+/*
+
+=========================================================
+* Now UI Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-kit-react
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit-react/blob/master/LICENSE.md)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "assets/css/bootstrap.min.css";
+import "assets/css/now-ui-kit.css";
+import "assets/demo/demo.css";
+import "assets/demo/nucleo-icons-page-styles.css";
+// pages for this kit
+
+import LandingPage from "views/pages/LandingPage.js";
+import ProductPage from "views/pages/ProductPage.js";
+import InformesPage from "views/pages/InformesPage.js";
+import NewsPage from "views/pages/NewsPage.js";
+import AboutPage from "views/pages/AboutPage.js";
+
+import LoginPage from "views/pages/LoginPage.js";
+
+ReactDOM.render(
+  <BrowserRouter forceRefresh={true}>
+    <Switch>
+        <Route path="/index" render={props => <LandingPage {...props}/>} />
+        <Route path="/landing-page" render={props => <LandingPage {...props} />}/>
+        <Route path="/informes-page" render={props => <InformesPage {...props} />} />
+        <Route path="/product-page" render={props => <ProductPage {...props} />} />
+        <Route path="/news-page" render={props => <NewsPage {...props} />} />
+        <Route path="/about-page" render={props => <AboutPage {...props} />} />
+        
+        <Route path="/login-page" render={props => <LoginPage {...props} />} />
+        <Redirect from="/" to="/index" />
+      
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
